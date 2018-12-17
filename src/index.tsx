2907b41e31f8dc40523d9b022e5478dc5f6dcc55
Vasily <gerasimovv1990@gmail.com>
2018-12-17 21:41:47 +0300

    init simple service worker

--- src/index.tsx
+++ src/index.tsx
@@ -5,3 +5,12 @@ import { App } from "./components/App";
 import "./styles/style.scss";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener("load", (): void => {
+        navigator.serviceWorker
+            .register('./src/service_worker.js')
+            .then(reg => console.log(reg))
+            .catch(err => console.log(err))
+    })
+}
